476cc9fcf96b9a5be6f160f2001a84e94c8472c9
Gegham Zakaryan <zakaryan.2004@outlook.com>
2019-07-02 10:48:22 +0400

    j5lte: Welcome to Evolution X!
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,3 @@
 LOCAL_PATH := device/samsung/j5lte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_j5lte.mk \
-	$(LOCAL_DIR)/rr_j5lte.mk \
-	$(LOCAL_DIR)/pa_j5lte.mk \
-	$(LOCAL_DIR)/lineage_j5lte.mk
+	$(LOCAL_DIR)/aosp_j5ltechn.mk
