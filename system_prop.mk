99459c1489feabefa285be498903a87aa733a688
Erfan Abdi <erfangplus@gmail.com>
2019-09-07 22:36:12 -0300

    msm8916-samsung: force disable bluetooth audio hal 2.0

--- system_prop.mk
+++ system_prop.mk
@@ -9,3 +9,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true
+
+# GPU Composition
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.early_phase_offset_ns=1500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000
+
+# Disable bluetooth HAL 2.0
+    persist.bluetooth.bluetooth_audio_hal.disabled=true \
